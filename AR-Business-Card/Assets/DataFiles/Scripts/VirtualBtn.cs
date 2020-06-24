ea63106b290fa6f9842257c2a2906e53ad9d64f2
Yash Jain <44024753+mvnsia@users.noreply.github.com>
2020-06-24 22:57:23 +0530

    fifth commit

--- AR-Business-Card/Assets/DataFiles/Scripts/VirtualBtn.cs
+++ AR-Business-Card/Assets/DataFiles/Scripts/VirtualBtn.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
+
 public class VirtualBtn : MonoBehaviour
 {
     public GameObject imagego, githubgo, resumego;
     // Start is called before the first frame update
     void Start()
     {
+        for(int i=0;i<vrb.Length;i++)
+        {
+            vrb[i].RegisterOnButtonPressed(OnButtonPressed);
+            vrb[i].RegisterOnButtonReleased(OnButtonReleased);
+        }
         imagego.SetActive(false);
         githubgo.SetActive(false);
         resumego.SetActive(false);
@@ -19,14 +25,38 @@ public class VirtualBtn : MonoBehaviour
         
     }
 
-    //public OnButtonPressed(VirtualButtonBehaviour vb)
-    //{
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        if(vb.VirtualButtonName== "ImageVB")
+        {
+            imagego.SetActive(true);
+            githubgo.SetActive(false);
+            resumego.SetActive(false);
+        }
 
-    //}
+        else if(vb.VirtualButtonName == "GithubVB")
+        {
+            imagego.SetActive(false);
+            githubgo.SetActive(true);
+            resumego.SetActive(false);
+        }
 
-    //public OnButtonReleased(VirtualButtonBehaviour vb)
-    //{
+        else if(vb.VirtualButtonName == "ResumeVB")
+        {
+            imagego.SetActive(false);
+            githubgo.SetActive(false);
+            resumego.SetActive(true);
+        }
 
-//    }
+        else
+        {
+            throw new UnityException(vb.VirtualButtonName + "virtual button not supported");
+        }
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        UnityEngine.Debug.Log("Virtual button released");
+     }
 
 }
