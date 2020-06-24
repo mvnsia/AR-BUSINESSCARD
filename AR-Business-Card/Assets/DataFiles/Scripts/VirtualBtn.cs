bd40a0c76fc1b63766fb5e4cd4df2b8a6422f027
Yash Jain <44024753+mvnsia@users.noreply.github.com>
2020-06-24 21:42:17 +0530

    fourth commit

---
+++ AR-Business-Card/Assets/DataFiles/Scripts/VirtualBtn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class VirtualBtn : MonoBehaviour
+{
+    public GameObject imagego, githubgo, resumego;
+    // Start is called before the first frame update
+    void Start()
+    {
+        imagego.SetActive(false);
+        githubgo.SetActive(false);
+        resumego.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //public OnButtonPressed(VirtualButtonBehaviour vb)
+    //{
+
+    //}
+
+    //public OnButtonReleased(VirtualButtonBehaviour vb)
+    //{
+
+//    }
+
+}
